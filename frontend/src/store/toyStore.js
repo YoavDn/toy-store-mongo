1ac8e42218eb1c4760cdeb6ebe99874531e6828b
yoav <yoavdan.m@gmail.com>
2022-07-15 12:44:18 +0300

    added filters with mongo

--- frontend/src/store/toyStore.js
+++ frontend/src/store/toyStore.js
@@ -3,37 +3,37 @@ import { toyService } from "../services/toy.service.js"
 export const toyStore = {
     state: {
         toys: [],
-         filterBy: {
-                query: '',
-                filters: {
-                    inStock: false,
-                    checkedLabels: [],
-                    sortBy: [],
+        filterBy: {
+            query: '',
+            filters: {
+                inStock: false,
+                checkedLabels: [],
+                sortBy: '',
 
-                },
-                labels: [
-                    'On wheels',
-                    'Box game',
-                    'Art',
-                    'Baby',
-                    'Doll',
-                    'Puzzle',
-                    'Outdoor',
-                    'Battery powered',
-                ],
-                sorts: [
-                    'price',
-                    'date',
-                    'name',
-                ]
             },
+            labels: [
+                'On wheels',
+                'Box game',
+                'Art',
+                'Baby',
+                'Doll',
+                'Puzzle',
+                'Outdoor',
+                'Battery powered',
+            ],
+            sorts: [
+                'price',
+                'date',
+                'name',
+            ]
+        },
 
     },
     mutations: {
         setToys: (state, { toys }) => state.toys = toys,
         deleteToy: (state, { toys }) => state.toys = toys,
         saveToy: (state, { toys }) => state.toys = toys,
-        setFilter: (state, {filterBy}) => state.filterBy = filterBy
+        setFilter: (state, { filterBy }) => state.filterBy = filterBy
         // setQuery: (state, { query }) => state.filterBy.query = query,
         // setFilter: ({ filterBy }, { filter }) => filterBy.filters[filter] = !filterBy.filters[filter],
         // setCheckedLabels: ({filterBy}, {checkedLabels}) =>  filterBy.filters.checkedLabels = checkedLabels,
@@ -51,15 +51,15 @@ export const toyStore = {
         // }
     },
     actions: {
-        loadToys: ( context ) => {
-            const  filterBy =  {...context.state.filterBy}
+        loadToys: (context) => {
+            const filterBy = { ...context.state.filterBy }
             toyService.query(filterBy)
                 .then(toys => {
                     context.commit({ type: 'setToys', toys })
                     return toys
                 })
         },
-    
+
         deleteToy: ({ commit }, { toy }) => {
             toyService.remove(toy)
                 .then(toys => commit({ type: 'deleteToy', toys }))
@@ -68,9 +68,9 @@ export const toyStore = {
             toyService.save(toy)
                 .then(toys => commit({ type: 'saveToy', toys }))
         },
-        setFilter({commit, dispatch}, {filterBy}) {
-            commit({type: 'setFilter', filterBy})
-                dispatch({type: 'loadToys'})
+        setFilter({ commit, dispatch }, { filterBy }) {
+            commit({ type: 'setFilter', filterBy })
+            dispatch({ type: 'loadToys' })
             // .then(toys => commit({type:'setToys', toys} ))
             // .catch(err => console.log('coulnt filter Toys'))
         }
